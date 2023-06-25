c3df4709c78f311b2da35fb6f775c2e4cdd64340
RFIXATE <s.cancinop@duocuc.cl>
2023-06-25 12:01:59 -0400

    1,4

--- test1.py
+++ test1.py
@@ -3,20 +3,33 @@ import random
 import json
 import requests
 
-rango_inicial=10
-rango_final=100
-#//PASO 1
-numero=random.randint(rango_inicial,rango_final)
+rango_inicial = 10
+rango_final = 100
+
+# Generar el número aleatorio
+numero = random.randint(rango_inicial, rango_final)
 print(numero)
 
-#//PASO 2
+# Construir el diccionario con los datos de la jugada
 jugada = {
-            'jugada':1,
-            'valor':numero
-        }
+    'jugador_id': '',
+    'juego_id': '',
+    'valor_jugada': numero
+}
 
-#//PASO 3
+# Enviar la solicitud POST al servidor
 url = 'http://172.31.29.110:8080/backend/api/jugada/1'
+enviar = requests.post(url, json=jugada)
+print('El servidor responde:', enviar.text)
 
-enviar = requests.post(url, json=json.dumps(jugada))
-print('El servidor responde: ', enviar.text)
+# Obtener los datos del servidor y asignarlos a las variables correspondientes
+respuesta = enviar.json()
+jugador_id = respuesta.get('jugador_id')
+juego_id = respuesta.get('juego_id')
+
+# Actualizar el diccionario de jugada con los valores recibidos del servidor
+jugada['jugador_id'] = jugador_id
+jugada['juego_id'] = juego_id
+
+# Imprimir la jugada con los valores asignados
+print('Jugada:', jugada)
--- test2.py
+++ test2.py
@@ -7,10 +7,14 @@ from datetime import datetime
 @post('/backend/api/jugada/1')
 
 def recibir_jugada():
+    data = request.json
+    jugador_id = data.get('jugador_id')
+    juego_id = data.get('juego_id')
+    valor_jugada = data.get('valor_jugada')
 
-    #jugada = request.forms.get('jugada')
-    jugada = 'TEST'
-    print (request.body)
-    return template('<b>Jugada 1 recibida {{jugada}}</b>!', jugada=jugada)
+    # Hacer algo con los datos recibidos, por ejemplo, almacenarlos en una base de datos
+
+    return template('<b>Jugada 1 recibida</b>! Jugador ID: {{jugador_id}}, Juego ID: {{juego_id}}, Valor de la jugada: {{valor_jugada}}',
+                    jugador_id=jugador_id, juego_id=juego_id, valor_jugada=valor_jugada)
 
 run(host='172.31.29.110', port=8080)
