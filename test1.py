48ca015d8a47ca482e5555b52566847c26954fce
RFIXATE <s.cancinop@duocuc.cl>
2023-06-25 22:40:49 -0400

    2,4

--- test1.py
+++ test1.py
@@ -1,95 +1,91 @@
 #MAQUINA CLIENTE CENTOS 8 172.31.29.110
 import random
+import json
 import requests
+import os
 
-def ingresar_jugador():
+server_url = 'http://192.168.24.128:8080/backend/api'
+
+def mostrar_menu():
+    print("---- Menú ----")
+    print("1. Ingresar ID del jugador")
+    print("2. Ingresar ID del juego")
+    print("3. Consultar juego disponible y estado del servidor")
+    print("4. Realizar jugada")
+    print("5. Consultar resultado del juego")
+    print("6. Salir")
+
+def ingresar_id_jugador():
     jugador_id = input("Ingrese el ID del jugador: ")
     return jugador_id
 
-def ingresar_juego():
+def ingresar_id_juego():
     juego_id = input("Ingrese el ID del juego: ")
     return juego_id
 
-def consultar_juego_disponible():
-    url = 'http://192.168.24.128:8080/backend/api/estado'
-    response = requests.get(url)
-    if response.status_code == 200:
-        data = response.json()
-        estado_servidor = data.get('estado_servidor')
-        juego_id = data.get('juego_id')
-        print(f"Estado del servidor: {estado_servidor}")
-        print(f"ID del juego disponible: {juego_id}")
-    else:
-        print("Error al consultar el estado del servidor")
+def consultar_juego_disponible_estado_servidor():
+    response = requests.get(f"{server_url}/juego_disponible_estado")
+    data = response.json()
+    print(f"Juego disponible: {data['juego_disponible']}")
+    print(f"Estado del servidor: {data['estado_servidor']}")
+    print(f"ID del juego en curso: {data['juego_id']}")
+    input("Presiona cualquier tecla para volver al menú...")
 
 def realizar_jugada(jugador_id, juego_id):
-    rango_inicial = 10
-    rango_final = 100
-    numero = random.randint(rango_inicial, rango_final)
-    print(f"Número generado: {numero}")
-
-    url = f'http://192.168.24.128:8080/backend/api/jugada/{juego_id}'
-
-    datos_jugada = {
+    valor_jugada = random.randint(1, 100)
+    jugada = {
         'jugador_id': jugador_id,
-        'valor_jugada': numero
+        'juego_id': juego_id,
+        'valor_jugada': valor_jugada
     }
-
-    response = requests.post(url, data=datos_jugada)
-
-    if response.status_code == 200:
-        print("Jugada enviada con éxito")
-    else:
-        print("Error al enviar la jugada")
+    response = requests.post(f"{server_url}/jugada", json=jugada)
+    print("Jugada realizada:")
+    print(response.text)
+    input("Presiona cualquier tecla para volver al menú...")
 
 def consultar_resultado_juego():
-    url = 'http://192.168.24.128:8080/backend/api/resultado'
-    response = requests.get(url)
-    if response.status_code == 200:
-        data = response.json()
-        jugadores = data.get('jugadores')
-        valores_jugadas = data.get('valores_jugadas')
-        jugador_ganador = data.get('jugador_ganador')
-        puntajes = data.get('puntajes')
+    response = requests.get(f"{server_url}/resultado")
+    resultado_juego = response.json()
+    print("---- Resultado del juego ----")
+    for i in range(len(resultado_juego['jugadores'])):
+        print(f"Jugador: {resultado_juego['jugadores'][i]} - Jugada: {resultado_juego['valores_jugadas'][i]}")
+    print(f"Jugador ganador: {resultado_juego['jugador_ganador']}")
+    print("Puntajes acumulados:")
+    for jugador, puntaje in resultado_juego['puntajes'].items():
+        print(f"{jugador}: {puntaje}")
+    input("Presiona cualquier tecla para volver al menú...")
 
-        print("Resultado del juego:")
-        print("Jugadores:", jugadores)
-        print("Valores de las jugadas:", valores_jugadas)
-        print("Jugador Ganador:", jugador_ganador)
-        print("Puntaje acumulado de los jugadores:", puntajes)
-    else:
-        print("Error al consultar el resultado del juego")
-
-def mostrar_menu():
-    print("----- MENU -----")
-    print("1. Ingresar ID del jugador")
-    print("2. Ingresar ID del juego")
-    print("3. Consultar juego disponible")
-    print("4. Realizar jugada")
-    print("5. Consultar resultado del juego")
-    print("0. Salir")
-
-jugador_id = ""
-juego_id = ""
-
-while True:
-    mostrar_menu()
-    opcion = input("Seleccione una opción: ")
-
-    if opcion == "1":
-        jugador_id = ingresar_jugador()
-    elif opcion == "2":
-        juego_id = ingresar_juego()
-    elif opcion == "3":
-        consultar_juego_disponible()
-    elif opcion == "4":
-        if jugador_id and juego_id:
-            realizar_jugada(jugador_id, juego_id)
+def main():
+    jugador_id = ""
+    juego_id = ""
+    
+    while True:
+        mostrar_menu()
+        opcion = input("Ingrese el número de opción: ")
+        if opcion == '1':
+            jugador_id = ingresar_id_jugador()
+            os.system('clear')  # Limpiar la consola en sistemas Unix/Linux
+        elif opcion == '2':
+            juego_id = ingresar_id_juego()
+            os.system('clear')  # Limpiar la consola en sistemas Unix/Linux
+        elif opcion == '3':
+            consultar_juego_disponible_estado_servidor()
+            os.system('clear')  # Limpiar la consola en sistemas Unix/Linux
+        elif opcion == '4':
+            if not jugador_id or not juego_id:
+                print("Debe ingresar el ID del jugador y el ID del juego primero.")
+                input("Presiona cualquier tecla para volver al menú...")
+            else:
+                realizar_jugada(jugador_id, juego_id)
+                os.system('clear')  # Limpiar la consola en sistemas Unix/Linux
+        elif opcion == '5':
+            consultar_resultado_juego()
+            os.system('clear')  # Limpiar la consola en sistemas Unix/Linux
+        elif opcion == '6':
+            print("¡Hasta luego!")
+            break
         else:
-            print("Ingrese el ID del jugador y del juego antes de realizar la jugada")
-    elif opcion == "5":
-        consultar_resultado_juego()
-    elif opcion == "0":
-        break
-    else:
-        print("Opción inválida. Intente nuevamente.")
+            print("Opción inválida. Intente nuevamente.")
+
+if __name__ == '__main__':
+    main()
