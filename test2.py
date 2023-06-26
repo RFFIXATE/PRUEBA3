d37eaf8a8b38bb8c8a3b02f47c607d1ae12161ed
RFIXATE <s.cancinop@duocuc.cl>
2023-06-26 15:39:20 -0400

    prueba 3 funcional

--- test2.py
+++ test2.py
@@ -14,7 +14,6 @@ estado_servidor = {
 }
 
 @post('/backend/api/jugada')
-
 def recibir_jugada():
     jugador_id = request.json.get('jugador_id')
     juego_id = request.json.get('juego_id')
@@ -33,11 +32,9 @@ def recibir_jugada():
 
     escribir_jugada_csv(jugador_id, juego_id, valor_jugada)
 
-    return template('<b>Jugada recibida correctamente. Jugador: {{jugador_id}}, Juego: {{juego_id}}, Valor: {{valor_jugada}}</b>', jugador_id=jugador_id, juego_id=juego_id, valor_jugada=valor_jugada)
-
+    return "Jugada recibida correctamente."
 
 @get('/backend/api/resultado')
-
 def obtener_resultado_juego():
     resultado = {
         'jugadores': jugadores,
@@ -48,12 +45,10 @@ def obtener_resultado_juego():
     return resultado
 
 @get('/backend/api/estado')
-
 def obtener_estado_servidor():
     return estado_servidor
 
 @post('/backend/api/juego')
-
 def iniciar_juego():
     estado_servidor['estado'] = 'ocupado'
     estado_servidor['juego_en_curso'] = random.randint(1, 100)
